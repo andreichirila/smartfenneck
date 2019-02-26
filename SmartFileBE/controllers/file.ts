78964c11d47a5e819abb02ef1a2e4e8a3c6cbe60
andrei.chirila <andrei.chirila@check24.de>
2019-02-26 16:37:16 +0100

    [sf-0002] prepares the basic infrastucture for uploading files

--- SmartFileBE/controllers/file.ts
+++ SmartFileBE/controllers/file.ts
@@ -1,15 +1,38 @@
 import * as fs from 'fs-extra';
+import * as multer from 'multer';
+
 import {FileModel} from '../models/file';
+import {uploadPath} from '../local_config/upload-path';
 
 export class FileController {
 
     private fileModel = new FileModel();
+    private multer = multer;
+    private uploadDIR = uploadPath;
     private fs = fs;
+    private multerUpload;
 
     constructor() {
+        this.multerUpload = this.multer({dest: this.uploadDIR}).single('file');
     }
 
-    public upload() {
-        this.fileModel.createFilesTable();
+    public upload(req, res) {
+        return this.multerUpload(req, res, (err) => {
+            if (err) {
+                // An error occurred when uploading
+                console.log(err);
+                return res.status(422).send('an Error occured')
+            }
+            // No error occured.
+            const path = req.file.path;
+            this.fileModel.insertFileInDB(
+                req.file.filename,
+                req.file.originalname,
+                req.file.size,
+                req.file.path,
+                req.file.mimetype,
+            );
+            return res.send('Upload Completed for ' + path).status('OK');
+        });
     }
 }
